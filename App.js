ceb0c1dff5339e68df61dbab784078b61e474e19
shengrong <shengrong_88@hotmail.com>
2020-12-08 10:30:53 +0800

    added FlatList read from BlockRGB

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
 
+const COLOURS = [
+  { red: 255, green: 128, blue: 0, id: "0"},
+  { red: 0, green: 128, blue: 255, id: "1"},
+  { red: 128, green: 0, blue: 255, id: "2"},
+]
+
 function HomeScreen() {
+  function renderItem ({ item }) {
+    return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
+  }
+
   return (
     <View style={styles.container}>
-      <BlockRGB red={180} green={60} blue={80} />
-      <BlockRGB red={100} green={200} blue={100} />
-      <BlockRGB red={150} green={20} blue={180} />
+      <FlatList 
+        style={{ width: "100%"}}
+        data={COLOURS}
+        renderItem={renderItem} />
     </View>
   );
 }
@@ -21,7 +32,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Kueh Lapis" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -32,6 +43,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "flex-start",
   },
 });
