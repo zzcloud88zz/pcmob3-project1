faf64845e98b2c4780fb167ee63980f72188ca96
shengrong <shengrong_88@hotmail.com>
2020-12-08 10:44:58 +0800

    added useState, colorArray, renderItem

--- App.js
+++ App.js
@@ -1,27 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, {useState} from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
 
-const COLOURS = [
-  { red: 255, green: 128, blue: 0, id: "0"},
-  { red: 0, green: 128, blue: 255, id: "1"},
-  { red: 128, green: 0, blue: 255, id: "2"},
-]
-
 function HomeScreen() {
-  function renderItem ({ item }) {
+  const [colorArray, setColorArray] = useState([
+    { red: 255, green: 128, blue: 0, id: "0" },
+    { red: 0, green: 128, blue: 255, id: "1" },
+    { red: 128, green: 0, blue: 255, id: "2" },
+  ]);
+
+  function renderItem({ item }) {
     return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
   }
 
   return (
     <View style={styles.container}>
-      <FlatList 
-        style={{ width: "100%"}}
-        data={COLOURS}
-        renderItem={renderItem} />
+      <FlatList
+        style={{ width: "100%" }}
+        data={colorArray}
+        renderItem={renderItem}
+      />
     </View>
   );
 }
